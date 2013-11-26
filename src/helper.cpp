ef402fc2d2e4c0838dfa6f108c7632b743d45fa4
Gianni Borghesan <gianni.borghesan@mech.kuleuven.be>
2013-11-26 14:07:52 +0100

    added functions to compute the base-expressed points and directions, and tested on point to point and ponit to line

--- src/helper.cpp
+++ src/helper.cpp
@@ -16,21 +16,44 @@ KDL::Rotation mlHom2KDLRot (const dynamicgraph::sot::MatrixHomogeneous &  M)
 namespace geometric_primitive
 {
 
-
+Expression<Vector>::Ptr ExpressInBase(const Point & p)
+{
+	return	p.o*p.p;
+}
+Expression<Vector>::Ptr ExpressInBase(const Versor & v)
+{
+	return rotation(v.o)*v.v;
+}
+Expression<Vector>::Ptr ExpressOriginInBase(const Plane & p)
+{
+	return p.o*p.p;
+}
+Expression<Vector>::Ptr ExpressOriginInBase(const Line & l)
+{
+	return l.o*l.p;
+}
+Expression<Vector>::Ptr ExpressDirectionInBase(const Plane & p)
+{
+	return rotation(p.o)*p.normal;
+}
+Expression<Vector>::Ptr ExpressDirectionInBase(const Line & l)
+{
+	return rotation(l.o)*l.dir;
+}
 using namespace KDL;
 /*for all the expressions we soppose that the frames are expressed in a common reference*/
 Expression<double>::Ptr point_point_distance(const Point & p1,const Point &  p2)
 {
-	Expression<Vector>::Ptr v1=p1.o*p1.p;
-	Expression<Vector>::Ptr v2=p2.o*p2.p;
+	Expression<Vector>::Ptr v1=ExpressInBase(p1);
+	Expression<Vector>::Ptr v2=ExpressInBase(p2);
 	return	norm(v1-v2);
 	//return norm(p1.p - inv(p1.o)*p2.o*p2.p);
 }
 Expression<double>::Ptr line_point_distance(const Point &  p, const Line & l)
 {
 	//i express everything in the line frame (l.o)
-	Expression<Vector>::Ptr dist_vect=l.p - inv(l.o)*p.o*p.p; //vector from l.p to p.p (in common frame)
-	Expression<Vector>::Ptr l_dir=l.dir; //direction vector in common frame
+	Expression<Vector>::Ptr dist_vect=ExpressOriginInBase(l) - ExpressInBase(p); //vector from l.p to p.p (in common frame)
+	Expression<Vector>::Ptr l_dir=ExpressDirectionInBase(l); //direction vector in common frame
 	// TODO check that l_dir is still a versor...
 	// TODO check if it works!
 	//l_dir=l_dir*(Constant<double>(1.0)/norm(l_dir));
@@ -41,13 +64,14 @@ Expression<double>::Ptr line_point_distance(const Point &  p, const Line & l)
 }
 Expression<double>::Ptr projection_of_point_on_line(const Point &  p, const Line & l)
 {
-	Expression<Vector>::Ptr a=l.o*l.p - p.o*p.p; //vector from l.p to p.p (in common frame)
-		Expression<Vector>::Ptr l_dir=l.o*l.dir; //direction vector in common frame
-		//check that l_dir is still a versor...
-		l_dir=l_dir*(Constant<double>(1)/norm(l_dir));
-	Expression<double>::Ptr f=dot(a,l_dir);//feature coordinate of the point of nearest distance
-	Expression<Vector>::Ptr pf=l.o*(l.dir*f);//point of nearest distance expressed in world
-	return norm(pf-l.o*l.p);
+	//i express everything in the line frame (l.o)
+	Expression<Vector>::Ptr dist_vect=l.p - inv(l.o)*p.o*p.p; //vector from l.p to p.p (in common frame)
+	Expression<Vector>::Ptr l_dir=l.dir; //direction vector in common frame
+	// TODO check that l_dir is still a versor...
+	// TODO check if it works!
+	//l_dir=l_dir*(Constant<double>(1.0)/norm(l_dir));
+	Expression<double>::Ptr proj=dot(dist_vect,l_dir);
+	return proj;
 }
 Expression<double>::Ptr distance_from_lines (const Line &  l1, const Line & l2)
 {
@@ -57,8 +81,8 @@ Expression<double>::Ptr distance_from_lines (const Line &  l1, const Line & l2)
 	 * NB THIS IS OK ONLY FOR SKEW (non parallel) LINES
 	 *
 	 * */
-	Expression<Vector>::Ptr a=l1.o*l1.dir;
-	Expression<Vector>::Ptr b=l2.o*l2.dir;
+	Expression<Vector>::Ptr a=rotation(l1.o)*l1.dir;
+	Expression<Vector>::Ptr b=rotation(l2.o)*l2.dir;
 	Expression<Vector>::Ptr c=l2.o*l2.p-l1.o*l1.p;
 	Expression<double>::Ptr num=abs(dot(c,cross(a,b)));
 	Expression<double>::Ptr den=norm(cross(a,b));
@@ -67,13 +91,23 @@ Expression<double>::Ptr distance_from_lines (const Line &  l1, const Line & l2)
 }
 
 
-
+//done by Francois
 Expression<double>::Ptr surface_point_distance(const Point & pt, const Plane & plan)
 {
   Expression<Vector>::Ptr oa = (plan.o * plan.p) - (pt.o * pt.p);
   Expression<Vector>::Ptr planNormal = plan.o * plan.normal;
-
   return  dot(oa, planNormal);
 }
-
+Expression<double>::Ptr angle_btw_versors (const Versor & v1, const Versor& v2)
+{
+	Expression<Vector>::Ptr vrs1=ExpressInBase(v1);
+	Expression<Vector>::Ptr vrs2=ExpressInBase(v2);
+	return KDL::acos(dot(vrs1,vrs2));
+}
+Expression<double>::Ptr angles_btw_planes (const Plane & pl1, const Plane & pl2)
+{
+	Expression<Vector>::Ptr vrs1=ExpressDirectionInBase(pl1);
+	Expression<Vector>::Ptr vrs2=ExpressDirectionInBase(pl2);
+	return KDL::acos(dot(vrs1,vrs2));
+}
 }
--- src/helper.h
+++ src/helper.h
@@ -41,6 +41,7 @@ Expression<double>::Ptr point_point_distance(const Point & p1,const Point& p2);
 Expression<double>::Ptr line_point_distance(const Point&  p, const Line& l);
 Expression<double>::Ptr projection_of_point_on_line(Point& p, const Line& l);
 Expression<double>::Ptr distance_from_lines (const Line& l1, const Line& l2);
+Expression<double>::Ptr surface_point_distance(const Point & p, const Plane & plan);
 
  /* Expression<double>::Ptr distance_o1_f1 (line l1, line l2);
  * Expression<double>::Ptr distance_o2_f2 (line l1, line l2);
@@ -50,10 +51,17 @@ Expression<double>::Ptr distance_from_lines (const Line& l1, const Line& l2);
  * Expression<double>::Ptr incident_angle (versor v, plane pl);
  * Expression<double>::Ptr angles_btw_planes (plane pl1, plane pl2);
  * */
+Expression<double>::Ptr angles_btw_planes (const Plane & pl1, const Plane & pl2);
+Expression<double>::Ptr angle_btw_versors (const Versor & v1, const Versor& v2);
 
-Expression<double>::Ptr surface_point_distance(const Point & p, const Plane & plan);
+//helper functions
+Expression<Vector>::Ptr ExpressInBase(const Point & p);
+Expression<Vector>::Ptr ExpressInBase(const Versor & v);
+Expression<Vector>::Ptr ExpressOriginInBase(const Plane & p);
+Expression<Vector>::Ptr ExpressOriginInBase(const Line & l);
+Expression<Vector>::Ptr ExpressDirectionInBase(const Plane & p);
+Expression<Vector>::Ptr ExpressDirectionInBase(const Line & l);
 }
-
 #endif // __SOT_FEATURE_EXPRESSIONGRAPH_HELPER_HH__
 
 
