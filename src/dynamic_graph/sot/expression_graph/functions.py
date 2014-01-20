c9a4070b04f73c4f38bd9c3ec8c6213497758222
Francois Keith <keith@lirmm.fr>
2014-01-20 15:35:33 +0100

    Add the creation of the inequality task in the script.
    Handle the cases where a value or a signal is attached to an IN signal.

--- src/dynamic_graph/sot/expression_graph/functions.py
+++ src/dynamic_graph/sot/expression_graph/functions.py
@@ -4,6 +4,14 @@ from dynamic_graph.sot.dyninv import  TaskInequality
 from dynamic_graph.sot.core import *
 from dynamic_graph.sot.expression_graph.types import *
 
+"""
+...
+"""
+def plugSignalOrValue(val, in_signal):
+  try:
+    plug(val, in_signal)
+  except:
+    in_signal.value = val
 
 """ 
 plug operational point to a feature
@@ -24,7 +32,7 @@ def plugFeatureToOpPoint(feature, index, op):
   try:
 	  plug(op.ref_frame, feature.signal('w_T_o'+index))
   except :
-	  feature.signal('w_T_o'+index).value = op.ref_frame
+	  plugSignalOrValue(op.ref_frame, feature.signal('w_T_o'+index))
 
 
 def plugOperationalPoint(feature, index, op):
@@ -43,20 +51,16 @@ def createFeaturePlaneVersorAngle(name, elmt1, elmt2):
 
 	# Get the position/Jacobian of the operational point for the dynamic entity
 	plugOperationalPoint(feature, '1', elmt1)
-	feature.norm1.value  = elmt1.normal
-	feature.p1.value     = elmt1.position
+	plugSignalOrValue(elmt1.normal,   feature.norm1)
+	plugSignalOrValue(elmt1.position, feature.p1)
 
 	#ground frame
 	plugOperationalPoint(feature, '2', elmt2)
-	feature.v2.value = elmt2.versor
+	plugSignalOrValue(elmt2.versor, feature.v2)
 
 	feature.reference.value = 0
 
-	# Define a task
-	task = Task('task'+name)
-	task.controlGain.value = 1
-	task.add(feature.name)
-	return(task, feature)
+	return feature
 
 
 def createFeatureVersorVersorAngle(name, elmt1, elmt2):
@@ -67,19 +71,32 @@ def createFeatureVersorVersorAngle(name, elmt1, elmt2):
 
 	# Get the position/Jacobian of the operational point for the dynamic entity
 	plugOperationalPoint(feature, '1', elmt1)
-	feature.v1.value = elmt1.versor
+	plugSignalOrValue(elmt1.versor, feature.v1)
 
 	#ground frame
 	plugOperationalPoint(feature, '2', elmt2)
-	feature.v2.value = elmt2.versor
+	plugSignalOrValue(elmt2.versor, feature.v2)
 
 	feature.reference.value = 0
 
-	# Define a task
-	task = Task('task'+name)
-	task.controlGain.value = 1
-	task.add(feature.name)
-	return(task, feature)
+	return feature
+
+""" Creates a point to point distance feature from two PointElement objects """
+def createFeaturePointPointDistance(name, elmt1, elmt2):
+	assert elmt1.typename == 'point'
+	assert elmt2.typename == 'point'
+
+	feature = FeaturePointToPointDistance(name)
+
+	# Get the position/Jacobian of the operational point for the dynamic entity
+	plugOperationalPoint(feature, '1', elmt1)
+	plugSignalOrValue(elmt1.position, feature.p1)
+
+	#ground frame
+	plugOperationalPoint(feature, '2', elmt2)
+	plugSignalOrValue(elmt2.position, feature.p2)
+
+	return feature
 
 
 def createFeaturePointToPoint(name, elmt1, elmt2):
@@ -90,60 +107,85 @@ def createFeaturePointToPoint(name, elmt1, elmt2):
 
 	# Get the position/Jacobian of the operational point for the dynamic entity
 	plugOperationalPoint(feature, '1', elmt1)
-	feature.p1.value = elmt1.position
+	plugSignalOrValue(elmt1.position, feature.p1)
 
 	#ground frame
 	plugOperationalPoint(feature, '2', elmt2)
-	feature.p2.value = elmt2.position
+	plugSignalOrValue(elmt2.position, feature.p2)
 
-	# Define a task
-	task = Task('task'+name)
-	task.controlGain.value = 1
+	return feature
+
+
+
+def createTask(name, feature, equality):
+	# Define a equality task
+	if equality:
+		task = Task('robot_task_'+name)
+		task.controlGain.value = 1
+
+	# Define a inequality task
+	else:
+		dt = 0.005
+		task = TaskInequality('robot_task_'+name)
+		task.dt.value=dt
+		task.controlGain.value = 1 * dt
 	task.add(feature.name)
-	return(task, feature)
 
+	return task
+
+
+
+
+def createTaskAndFeaturePointToPoint(name, elmt1, elmt2, equality=True):
+
+	feature = createFeaturePointToPoint(name, elmt1, elmt2)
+	task = createTask(name, feature, equality)
+	return (task, feature)
 
 """
 Main method to create the task and the feature
 """
-def createTaskAndFeature(name, elmt1, elmt2, operation):
+def createTaskAndFeature(name, elmt1, elmt2, operation, equality=True):
 	if operation == 'distance':
 		# line / line
 		if   elmt1.typename == 'line' and elmt2.typename == 'line':
-			return createFeatureLineLineDistance(name, elmt1, elmt2)
+			feature = createFeatureLineLineDistance(name, elmt1, elmt2)
 		# point / surface
 		elif elmt1.typename == 'point' and elmt2.typename == 'surface':
-			return createFeatureSurfacePointDistance(name, elmt2, elmt1)
+			feature = createFeatureSurfacePointDistance(name, elmt2, elmt1)
 		elif elmt1.typename == 'surface' and elmt2.typename == 'point':
-			return createFeatureSurfacePointDistance(name, elmt1, elmt2)
+			feature = createFeatureSurfacePointDistance(name, elmt1, elmt2)
 		# point / line
 		elif elmt1.typename == 'line' and elmt2.typename == 'point':
-			return createFeatureLinePointDistance(name, elmt1, elmt2)
+			feature = createFeatureLinePointDistance(name, elmt1, elmt2)
 		elif elmt1.typename == 'point' and elmt2.typename == 'line':
-			return createFeatureLinePointDistance(name, elmt2, elmt1)
+			feature = createFeatureLinePointDistance(name, elmt2, elmt1)
 		# point / point
 		elif elmt1.typename == 'point' and elmt2.typename == 'point':
-			return createFeaturePointPointDistance(name, elmt1, elmt2)
+			feature = createFeaturePointPointDistance(name, elmt1, elmt2)
 
 		# not handled.
 		else :
-			print 'Unable to compute the angle between ' + elmt1.typename + '/' + elmt2.typename 
+			raise TypeError('Unable to compute the angle between ' + elmt1.typename + '/' + elmt2.typename)
 
 	elif operation == 'angle':
 		# plane / plane
 		if   elmt1.typename == 'plane' and elmt2.typename == 'plane':
-			return createFeaturePlanePlaneAngle(name, elmt1, elmt2)
+			feature = createFeaturePlanePlaneAngle(name, elmt1, elmt2)
 		# plane / versor
 		elif elmt1.typename == 'plane' and elmt2.typename == 'versor':
-			return createFeaturePlaneVersorAngle(name, elmt1, elmt2)
+			feature = createFeaturePlaneVersorAngle(name, elmt1, elmt2)
 		elif elmt1.typename == 'versor' and elmt2.typename == 'plane':
-			return createFeaturePlaneVersorAngle(name, elmt1, elmt2)
+			feature = createFeaturePlaneVersorAngle(name, elmt1, elmt2)
 		# versor / versor
 		elif elmt1.typename == 'versor' and elmt2.typename == 'versor':
-			return createFeatureVersorVersorAngle(name, elmt1, elmt2)
+			feature = createFeatureVersorVersorAngle(name, elmt1, elmt2)
 		else:
-			print 'Unable to compute the angle between ' + elmt1.typename  + '/' + elmt2.typename 
+			raise TypeError('Unable to compute the angle between ' + elmt1.typename  + '/' + elmt2.typename)
 
 	else:
-			print "operation " +  operation + " unknown"
+			raise TypeError("operation " +  operation + " unknown")
+
+	task = createTask(name, feature, equality)
+	return (task, feature)
 
